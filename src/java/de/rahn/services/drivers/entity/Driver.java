0a9e4cc473b6fdea04a4781d7c60eef7dd07d58e
Frank W. Rahn <frank@frank-rahn.de>
2011-07-30 19:36:58 +0000

    Kleinere Änderungen

--- src/java/de/rahn/services/drivers/entity/Driver.java
+++ src/java/de/rahn/services/drivers/entity/Driver.java
@@ -16,6 +16,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
+import javax.persistence.Persistence;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
@@ -67,7 +68,11 @@ public class Driver {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + (cars == null ? 0 : cars.hashCode());
+		if (cars != null && Persistence.getPersistenceUtil().isLoaded(cars)) {
+			result = prime * result + cars.hashCode();
+		} else {
+			result = prime * result;
+		}
 		result = prime * result + (firstname == null ? 0 : firstname.hashCode());
 		result = prime * result + (id == null ? 0 : id.hashCode());
 		result = prime * result + (name == null ? 0 : name.hashCode());
@@ -93,8 +98,12 @@ public class Driver {
 			if (other.cars != null) {
 				return false;
 			}
-		} else if (!cars.equals(other.cars)) {
-			return false;
+		} else {
+			if (Persistence.getPersistenceUtil().isLoaded(cars) && Persistence.getPersistenceUtil().isLoaded(other.cars)) {
+				if (!cars.equals(other.cars)) {
+					return false;
+				}
+			}
 		}
 		if (firstname == null) {
 			if (other.firstname != null) {
