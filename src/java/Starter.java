0b13e2dc3dfbafdd1b20100d853298094fb4f5e0
Frank W. Rahn <frank@frank-rahn.de>
2011-09-18 15:19:29 +0000

    Spring Security mit einer Webanwendung

--- src/java/Starter.java
+++ src/java/Starter.java
@@ -1,11 +1,26 @@
+import static org.slf4j.LoggerFactory.getLogger;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import org.slf4j.Logger;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import de.rahn.app.Application;
 
 /**
  * Die Klasse zum Starten der Anwendung.
  * @author Frank W. Rahn
  */
 public class Starter {
+
+	private static final Logger logger = getLogger(Application.class);
 
 	/**
 	 * @param args die Argumente der Anwendung
@@ -15,6 +30,28 @@ public class Starter {
 		ApplicationContext ctx =
 			new ClassPathXmlApplicationContext(
 				"/META-INF/spring/context-app.xml");
+
+		try {
+			// Einlesen der Authentisierungsdaten
+			BufferedReader in =
+				new BufferedReader(new InputStreamReader(System.in));
+			System.out.println("Please enter your username:");
+			String name = in.readLine();
+			System.out.println("Please enter your password:");
+			String password = in.readLine();
+
+			// Authentifizierung des Benutzers
+			AuthenticationManager authenticationManager =
+				ctx.getBean(BeanIds.AUTHENTICATION_MANAGER,
+					AuthenticationManager.class);
+			Authentication request =
+				new UsernamePasswordAuthenticationToken(name, password);
+			Authentication result = authenticationManager.authenticate(request);
+			SecurityContextHolder.getContext().setAuthentication(result);
+		} catch (Exception exception) {
+			logger.error("Fehler bei der Authentifizierung", exception);
+			return;
+		}
 
 		// Aufruf der Anwendung
 		Runnable service = ctx.getBean("application", Runnable.class);
