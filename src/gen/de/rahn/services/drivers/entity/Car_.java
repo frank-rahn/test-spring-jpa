16f6490f164d76bd552d7ec6dbbcd6e8a68bf69a
Frank W. Rahn <frank@frank-rahn.de>
2012-10-07 16:37:10 +0200

    Neue Versionen der Libraries

--- src/gen/de/rahn/services/drivers/entity/Car_.java
+++ src/gen/de/rahn/services/drivers/entity/Car_.java
@@ -1,8 +1,10 @@
 package de.rahn.services.drivers.entity;
 
+import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
 @StaticMetamodel(Car.class)
 public abstract class Car_ {
 
