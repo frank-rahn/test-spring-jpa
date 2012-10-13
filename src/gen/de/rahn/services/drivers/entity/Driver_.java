a3bbecef5a96721b56c580b4557e710d0475461c
Frank W. Rahn <frank@frank-rahn.de>
2012-10-13 20:18:07 +0200

    Änderungen wegen Eclipse 4.2 und OpenJDK 1.7 Überflüssige Libraries entfernt

--- src/gen/de/rahn/services/drivers/entity/Driver_.java
+++ src/gen/de/rahn/services/drivers/entity/Driver_.java
@@ -1,9 +1,12 @@
 package de.rahn.services.drivers.entity;
 
+import javax.annotation.Generated;
 import javax.persistence.metamodel.SetAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor", date = "2012-10-13T20:09:02.095+0200")
+@SuppressWarnings("all")
 @StaticMetamodel(Driver.class)
 public abstract class Driver_ {
 
