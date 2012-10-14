9bf28234fc5114b71abbaba939c6df3347bd830b
Frank W. Rahn <frank@frank-rahn.de>
2012-10-14 12:06:38 +0200

    Bugfix: Classpath

---
+++ src/gen/de/rahn/services/drivers/entity/Car_.java
@@ -0,0 +1,16 @@
+package de.rahn.services.drivers.entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor", date = "2012-10-14T12:01:57.448+0200")
+@SuppressWarnings("all")
+@StaticMetamodel(Car.class)
+public abstract class Car_ {
+
+	public static volatile SingularAttribute<Car, String> id;
+	public static volatile SingularAttribute<Car, String> type;
+
+}
+
---
+++ src/gen/de/rahn/services/drivers/entity/Driver_.java
@@ -0,0 +1,19 @@
+package de.rahn.services.drivers.entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SetAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor", date = "2012-10-14T12:01:57.449+0200")
+@SuppressWarnings("all")
+@StaticMetamodel(Driver.class)
+public abstract class Driver_ {
+
+	public static volatile SingularAttribute<Driver, Long> id;
+	public static volatile SetAttribute<Driver, Car> cars;
+	public static volatile SingularAttribute<Driver, String> name;
+	public static volatile SingularAttribute<Driver, String> firstname;
+
+}
+
